0edf64a732812aa9db2f4b174274756f85e8f08f
imswastik68 <swastikbehera30@gmail.com>
2025-03-27 23:53:08 +0530

    final_changes

---
+++ app/(main)/transaction/create/page.tsx
@@ -0,0 +1,55 @@
+import { getUserAccounts } from "@/actions/dashboard";
+import { defaultCategories } from "@/data/categories";
+import { AddTransactionForm } from "../_components/transaction-form";
+import { getTransaction } from "@/actions/transaction";
+
+interface SearchParams {
+  edit?: string;
+}
+
+interface Transaction {
+  type: string;
+  amount: number;
+  description: string;
+  accountId: string;
+  category: string;
+  date: string;
+  isRecurring: boolean;
+  recurringInterval?: string;
+}
+
+export default async function AddTransactionPage({ searchParams }: { searchParams: SearchParams }) {
+  const accounts = (await getUserAccounts()) || [];
+  const editId = searchParams?.edit;
+
+  let initialData = null;
+  if (editId) {
+    const transaction = await getTransaction(editId);
+    if (transaction) {
+      initialData = {
+        type: (transaction as Transaction).type || '',
+        amount: transaction.amount || 0,
+        description: (transaction as Transaction).description || '',
+        accountId: (transaction as Transaction).accountId || '',
+        category: (transaction as Transaction).category || '',
+        date: (transaction as Transaction).date || '',
+        isRecurring: (transaction as Transaction).isRecurring || false,
+        recurringInterval: (transaction as Transaction).recurringInterval || undefined,
+      };
+    }
+  }
+
+  return (
+    <div className="max-w-3xl mx-auto px-5">
+      <div className="flex justify-center md:justify-normal mb-8">
+        <h1 className="text-5xl gradient-title ">Add Transaction</h1>
+      </div>
+      <AddTransactionForm
+        accounts={accounts}
+        categories={defaultCategories}
+        editMode={!!editId}
+        initialData={initialData}
+      />
+    </div>
+  );
+}
