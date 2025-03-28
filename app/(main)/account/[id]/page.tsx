20c4be976f405cc837b5b194030456f2b97f7a07
imswastik68 <swastikbehera30@gmail.com>
2025-03-28 19:53:41 +0530

    .

--- app/(main)/account/[id]/page.tsx
+++ app/(main)/account/[id]/page.tsx
@@ -1,15 +1,15 @@
-import { Suspense } from "react";
 import { getAccountWithTransactions } from "@/actions/account";
 import { BarLoader } from "react-spinners";
 import { TransactionTable } from "../_components/transaction-table";
 import { notFound } from "next/navigation";
 import { AccountChart } from "../_components/account-chart";
+import { Suspense } from "react";
 
-interface Params {
-  id: string;
-}
+type PageProps<T> = {
+  params: T;
+};
 
-export default async function AccountPage({ params }: { params: Params }) {
+export default async function AccountPage({ params }: PageProps<{ id: string }>) {
   const accountData = await getAccountWithTransactions(params.id);
 
   if (!accountData) {
